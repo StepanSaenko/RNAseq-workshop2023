48c1ccf14d932cea002ac080af5234ca42ecfa73
stollec <stollec@gmail.com>
2024-03-13 10:27:51 +0100

    Add files via upload

---
+++ RNAseq/05.GO/results/go_enrichment_analysis_fisher.brains.R
@@ -0,0 +1,161 @@
+#!/usr/bin/env Rscript
+##############################################################################
+# Author: Joe Colgan                   Program: go_enrichment_analysis_fisher.R
+# modified by Eckart Stolle, 2024
+# Date: 16/08/2017
+##############################################################################
+
+setwd('/scratch/rnaseq2023/estolle/go')
+
+# Load libraries; install from scratch if needed
+libraries <- c("topGO",
+               "lintr",
+               "lattice",
+		"Rgraphviz")
+for (lib in libraries) {
+    if (require(package = lib, character.only = TRUE)) {
+        print("Successful")
+    } else {
+        print("Installing")
+        source("https://bioconductor.org/biocLite.R")
+        avebiocLite(pkgs = lib)
+        library(lib, character.only = TRUE)
+    }
+}
+
+## Step One: Input files, define output and objects for running topGO: 
+# Load in genelist and database files.  
+
+## Step One:
+## Define input:
+input <- "/scratch/rnaseq2023/estolle/go/Apis.mellifera.GOterms.tsv"
+
+## GO annotations
+gene_to_go_mapping_file <- input
+
+## genes overlapping with regions of significantly low nucleotide diversity:
+gene_input <- "brains.txt"
+gene_list <- scan(file = paste("genes_of_interest/",
+                               gene_input,
+                               sep = ""),
+                  as.character())
+
+## Define node size:
+node_size <- 50
+
+output_directory <- paste("results.topGO.brains/go_term_output_",
+                          node_size,
+                          "_fisher",
+                          sep = "")
+if (file.exists(output_directory)) {
+  stop("The output directory:", output_directory, ", already exists",
+       "Let's avoid overwriting")
+} else {
+  dir.create(output_directory,
+             recursive = TRUE)
+}
+
+## Read in input file:
+## Read in GO annotations: 
+gene_to_go_mapping <- readMappings(file = gene_to_go_mapping_file)
+gene_universe <- names(gene_to_go_mapping)
+
+genes_of_interest <- as.character(gene_list)
+
+genelist <- factor(as.integer(gene_universe %in% genes_of_interest))
+names(genelist) <- gene_universe
+
+## Steps Two and Three: Create topGO Object & run tests for GO term enrichment
+# We create a topGO object for each GO term
+# We perform two statistical tests:
+# 1. A ks test using the topGO 'weight01' algorithm
+# 2. A Fisher's exact test using the topGO 'weight01' algoritm
+#We combine the output of each test. 
+#We filter out enriched terms.
+#We do this for each of the three GO categories (ie. Biological process, Molcular Function, Cellular Component):
+
+for (go_category in c("BP", "MF", "CC")) {
+  # STEP TWO
+  ## Build the GOdata object in topGO
+  my_go_data <- new("topGOdata",
+                    description = paste("GOtest", go_category, sep = "_"),
+                    ontology    = go_category,
+                    allGenes    = genelist,
+                    gene2GO     = gene_to_go_mapping,
+                    annot       = annFUN.gene2GO,
+                    nodeSize    = node_size) # Modify to reduce/increase stringency.
+  # STEP THREE
+  ## Calculate ks test using 'weight01' algorithm:
+  result_weight_ks <- runTest(object    = my_go_data,
+                              algorithm = "weight01",
+                              statistic = "ks")
+  ## Calculate fisher exact test using 'weight01' algorithm:
+  result_weight_fisher <- runTest(object    = my_go_data,
+                                  algorithm = "weight01",
+                                  statistic = "fisher")
+  ## Combine results from statistical tests:
+  result_weight_output <- GenTable(object       = my_go_data,
+                                   weight_ks     = result_weight_ks,
+                                   weight_fisher = result_weight_fisher,
+                                   orderBy       = "weight_fisher",
+                                   topNodes      = length(score(result_weight_fisher)))
+  ## Correct ks test for multiple testing:
+  result_weight_output$weight_ks <- as.numeric(result_weight_output$weight_ks)
+  result_weight_output$weight_fisher <- as.numeric(result_weight_output$weight_fisher)
+  result_weight_output$weight_ks_adjusted <- p.adjust(p = result_weight_output$weight_ks,
+                                                      method = c("BH"))
+  result_weight_output$weight_fisher_adjusted <- p.adjust(p = result_weight_output$weight_fisher,
+                                                          method = c("BH"))
+  ## Write to output:
+  write.table(x         = result_weight_output,
+              file      = file.path(output_directory,
+                                    paste(go_category,
+                                          "raw.tsv",
+                                          sep = "_")),
+              row.names = FALSE,
+              sep       = "\t",
+              quote = FALSE)
+
+   ## write sign terms
+result_weight_output2 <- result_weight_output[which(result_weight_output$weight_fisher<=0.05),]
+#0.001
+#result_weight_output3[which(result_weight_output$weight_fisher_adjusted<=0.05),]
+#save first top 50 ontolgies sorted by adjusted pvalues
+  write.table(x         = result_weight_output2[1:50,],
+              file      = file.path(output_directory,
+                                    paste(go_category,
+                                          "top50.tsv",
+                                          sep = "_")),
+              row.names = FALSE,
+              sep       = "\t",
+              quote = FALSE)
+
+
+
+# PLOT the GO hierarchy plot: the enriched GO terms are colored in yellow/red according to significance level
+#BiocManager::install("Rgraphviz")
+pdf(file=file.path(output_directory,paste(go_category,"topGOPlot_fullnames.pdf",sep = "_")), height=12, width=12, paper='special', pointsize=18)
+#showSigOfNodes(my_go_data, score(result_weight_output$weight_fisher), useInfo = "none", sigForAll=FALSE, firstSigNodes=2,.NO.CHAR=50))
+#dev.off()
+
+myterms = result_weight_output$GO.ID
+mygenes = genesInTerm(my_go_data, myterms)
+
+var=c()
+for (i in 1:length(myterms))
+{
+myterm=myterms[i]
+mygenesforterm= mygenes[myterm][[1]]
+mygenesforterm=paste(mygenesforterm, collapse=',')
+var[i]=paste("GOTerm",myterm,"genes-",mygenesforterm)
+}
+  write.table(x         = var,
+              file      = file.path(output_directory,
+                                    paste(go_category,
+                                          "genetoGOmapping.txt",
+                                          sep = "_")),
+              row.names = FALSE,
+              sep       = "\t",
+              quote = FALSE)
+
+}
---
+++ RNAseq/05.GO/results/go_term_extraction.Amel.R
@@ -0,0 +1,155 @@
+#!/usr/bin/env Rscript
+#
+#title: "extract go terms from ensembl metazoa"
+#output: extract_ensembl.html
+#author: Joe Colgan (joscolgan)
+
+
+## Introduction:
+#This script takes a tab-delimited file containing two columns:
+#- Column 1 - query (QuerySpecies) protein ID
+#- Column 2 - ensembl reference eg Apis (RefSpecies) protein ID for predicted homolog
+#The script uses biomaRt to obtained Gene Ontology terms assigned to homologs in Apis mellifera.
+#The script matches Euglossa homologs to Gene Ontology assignaed to Apis homologs and writes a dataframe to file.
+#The script performs the same analysis using Drosophila melanogaster.
+
+#1. Load libraries:
+
+require(biomaRt)
+require(dplyr)
+require(tidyr)
+#inputfile = "data/EuglossaDilemma_ApisMellifera_1v1_orthologs.sorted.tsv"
+inputfile = "/scratch/rnaseq2023/estolle/go/Apis.mellifera.transcriptIDsfromGTF2.txt"    #replace with path to file (eg on commandline via sed)
+
+
+#2. Load ensembl database:
+
+
+## replace ##XXXX to uncomment your required version
+ensembl <- useEnsemblGenomes(biomart = "metazoa_mart", dataset = "amellifera_eg_gene")
+##BTER ensembl <- useEnsemblGenomes(biomart = "metazoa_mart", dataset = "bterrestris_eg_gene")
+##BIMP ensembl <- useEnsemblGenomes(biomart = "metazoa_mart", dataset = "bimpatiens_eg_gene")
+##NVIT ensembl <- useEnsemblGenomes(biomart = "metazoa_mart", dataset = "nvitripennis_eg_gene")                            
+
+
+#3. Extract Gene Ontology terms directly assigned to RefSpecies (eg Apis):
+
+
+## Extract gene coordinates:
+ref_ncbi <- getBM(attributes = c('ensembl_gene_id',
+                                  	'ensembl_peptide_id',
+                                   	'entrezgene_id',
+                                   	'chromosome_name',
+                                   	'start_position',
+                                   	'end_position',
+                                  	'go_id',
+                                  	'name_1006'),
+                    				 mart = ensembl)
+
+
+
+#4.Read in table containing list of QuerySpecies and RefSpecies homologs: 
+ 
+
+
+query_data <- read.table(file = inputfile, header = FALSE,
+                            col.names = c("query_prot", "ref_prot"))
+
+## Remove extension from protein ID:
+query_data$ref_prot <- gsub(pattern = "[.]1", replacement = "",
+                                query_data$ref_prot)
+
+
+#5. Subset ensembl gene id, GO id and GO term description:
+
+
+## Match RefSpecies terms in both dataframes and add associated QuerySpecies protein ids to original dataframe:
+## Match Drosophila gene id in both dataframes and add RefSpecies gene ID to dataframe containing GO terms:
+
+ref_ncbi$query_prot <- query_data[match(ref_ncbi$ensembl_peptide_id,
+                                               query_data$ref_prot), ]$query_prot
+
+query_ref_go_terms <- ref_ncbi %>%
+        select(c(query_prot, go_id))
+
+## Remove blank lines:
+query_ref_go_terms <- subset(query_ref_go_terms,
+                            query_prot != "")
+
+query_ref_go_terms <- subset(query_ref_go_terms,
+                            go_id != "")
+
+
+#6. Write to file:
+
+
+## Create output directory:
+# dir.create(path = "results")
+
+## Write to file:
+write.table(query_ref_go_terms, file = paste(inputfile,'.GO.Amel.raw.txt', sep = ''), quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
+##BTER write.table(query_ref_go_terms, file = paste(inputfile,'.GO.Bter.raw.txt', sep = ''), quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
+##BIMP write.table(query_ref_go_terms, file = paste(inputfile,'.GO.Bimp.raw.txt', sep = ''), quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
+##NVIT write.table(query_ref_go_terms, file = paste(inputfile,'.GO.Nvit.raw.txt', sep = ''), quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
+
+
+#7. To get RefSpecies genes assigned with GO terms for Drosophila orthologs:
+
+
+dros_ensembl <- useEnsemblGenomes(biomart = "metazoa_mart", dataset = "dmelanogaster_eg_gene")
+
+
+#8. Extract Gene Ontology terms for Drosophila as well as the :
+
+
+## Check attributes:
+listAttributes(dros_ensembl)[521:530, ]
+
+## Extract gene coordinates:
+## Biomart doesnt allow to directly access GO terms and homolog information unfortunately:
+
+dros_go_terms <- getBM(attributes = c('ensembl_gene_id', 'go_id', 'name_1006'), mart = dros_ensembl)
+
+## Independetly extract Drosophila gene ID and RefSpecies homolog:
+dros_ref_homologs <- getBM(attributes = c('ensembl_gene_id', 'amellifera_eg_homolog_ensembl_gene', 'amellifera_eg_homolog_ensembl_peptide'), mart = dros_ensembl)
+##BTER dros_ref_homologs <- getBM(attributes = c('ensembl_gene_id', 'bterrestris_eg_homolog_ensembl_gene', 'bterrestris_eg_homolog_ensembl_peptide'), mart = dros_ensembl)
+##BIMP dros_ref_homologs <- getBM(attributes = c('ensembl_gene_id', 'bimpatiens_eg_homolog_ensembl_gene', 'bimpatiens_eg_homolog_ensembl_peptide'), mart = dros_ensembl)
+##NVIT dros_ref_homologs <- getBM(attributes = c('ensembl_gene_id', 'nvitripennis_eg_homolog_ensembl_gene', 'nvitripennis_eg_homolog_ensembl_peptide'),  mart = dros_ensembl)
+
+## Match Drosophila gene id in both dataframes and add Apis mellifera gene ID to dataframe containing GO terms:
+dros_go_terms$amellifera_eg_homolog_ensembl_peptide <- dros_ref_homologs[match(dros_go_terms$ensembl_gene_id, dros_ref_homologs$ensembl_gene_id), ]$amellifera_eg_homolog_ensembl_peptide
+##BTER dros_go_terms$bterrestris_eg_homolog_ensembl_peptide <- dros_ref_homologs[match(dros_go_terms$ensembl_gene_id, dros_ref_homologs$ensembl_gene_id), ]$bterrestris_eg_homolog_ensembl_peptide
+##BIMP dros_go_terms$bimpatiens_eg_homolog_ensembl_peptide <- dros_ref_homologs[match(dros_go_terms$ensembl_gene_id, dros_ref_homologs$ensembl_gene_id), ]$bimpatiens_eg_homolog_ensembl_peptide
+##NVIT dros_go_terms$nvitripennis_eg_homolog_ensembl_peptide <- dros_ref_homologs[match(dros_go_terms$ensembl_gene_id, dros_ref_homologs$ensembl_gene_id), ]$nvitripennis_eg_homolog_ensembl_peptide
+
+
+dros_go_terms$query_prot <- query_data[match(dros_go_terms$amellifera_eg_homolog_ensembl_peptide, query_data$ref_prot), ]$query_prot
+##BTER dros_go_terms$query_prot <- query_data[match(dros_go_terms$bterrestris_eg_homolog_ensembl_peptide, query_data$ref_prot), ]$query_prot
+##BIMP dros_go_terms$query_prot <- query_data[match(dros_go_terms$bimpatiens_eg_homolog_ensembl_peptide, query_data$ref_prot), ]$query_prot
+##NVIT dros_go_terms$query_prot <- query_data[match(dros_go_terms$nvitripennis_eg_homolog_ensembl_peptide,  query_data$ref_prot), ]$query_prot
+
+
+
+## Rearrange order of Gene Ontology terms:
+query_dros_go_terms <- dros_go_terms %>%
+        select(c(query_prot, go_id))
+
+## Remove blank lines:
+query_dros_go_terms <- subset(query_dros_go_terms,
+                             query_prot != "")
+query_dros_go_terms <- subset(query_dros_go_terms,
+                             go_id != "")
+
+
+#9. Write to file:
+
+## Create output directory:
+# dir.create(path = "results")
+
+## Write to file:
+write.table(query_dros_go_terms, file = paste(inputfile,'.GO.Amel-Dmel.raw.txt', sep = ''), quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
+##BTER write.table(query_dros_go_terms, file = paste(inputfile,'.GO.Bter-Dmel.raw.txt', sep = ''), quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
+##BIMP write.table(query_dros_go_terms, file = paste(inputfile,'.GO.Bimp-Dmel.raw.txt', sep = ''), quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
+##NVIT write.table(query_dros_go_terms, file = paste(inputfile,'.GO.Nvit-Dmel.raw.txt', sep = ''), quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
+
+
